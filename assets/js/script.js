b390c767395abf5ce767b3bf87d7ba15c772ea98
ouiam <ahmwiam9gmail.com>
2025-05-05 16:44:28 +0200

    000

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,19 +8,32 @@ const burger = document.getElementById('burger-menu');
     });
 
     const track = document.querySelector('.carousel-track');
+    const slide = document.querySelector('.carousel');
     const btnNext = document.querySelector('.carousel-btn.next');
     const btnPrev = document.querySelector('.carousel-btn.prev');
     let index = 0;
-  
+
+    
+    
     btnNext.addEventListener('click', () => {
       const slideWidth = track.querySelector('img').clientWidth;
       if (index < track.children.length - 1) index++;
-      track.style.transform = `translateX(-${slideWidth * index}px)`;
+
+      if(slideWidth * index < slide.clientWidth){
+        track.style.transform = `translateX(-${slideWidth * index}px)`;
+      }else{
+        slide.clientWidth = slideWidth + index
+      }
     });
   
     btnPrev.addEventListener('click', () => {
       const slideWidth = track.querySelector('img').clientWidth;
+    
       if (index > 0) index--;
+
+      console.log(slideWidth * index)
+      console.log(slide.clientWidth - slideWidth * index)
+
       track.style.transform = `translateX(-${slideWidth * index}px)`;
     });
   
